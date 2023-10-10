cc1cb47598da6269c0fc27d17b698ac287c38a90
Paz Saragi <76964496+pazsaragi@users.noreply.github.com>
2023-10-10 19:30:52 +0000

    add go implementation of async await

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"codespaces-test/async"
+	"fmt"
+	"time"
+)
+
+// Runs async and returns a channel
+func asyncFunc() chan int {
+	r := make(chan int)
+	go func() {
+		time.Sleep(3 * time.Second)
+		// writes to channel
+		// once async function is done
+		r <- 1
+		fmt.Println("Done ...")
+	}()
+
+	return r
+}
 
 func main() {
-	messages := make(chan string)
-
-	go func() { messages <- "ping" }()
-
-	msg := <-messages
-	fmt.Println(msg)
+	fmt.Println("Start ...")
+	r := async.Exec(func() interface{} {
+		return asyncFunc()
+	})
+	fmt.Println("Async task runnin ...")
+	val := r.Await()
+	fmt.Println(val)
 }
